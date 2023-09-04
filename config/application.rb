836916139c0ab71443b18f86587236324d0d8d7b
Walczyk <k.walczyk@fibcode.com>
2023-09-04 19:47:23 +0200

    add internationalization to user

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ThriveBudget
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.available_locales = %i[en pl]
+    config.i18n.default_locale = :pl
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
