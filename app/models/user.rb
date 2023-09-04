c905eba78bfe551ab63054093a5d726f8a98fcd0
Walczyk <k.walczyk@fibcode.com>
2023-09-04 19:15:41 +0200

    validate user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_confirmation_of :password
+
   validates :name, presence: true
   validates :surname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
